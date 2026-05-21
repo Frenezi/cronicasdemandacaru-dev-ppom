209d2a875db1fcfed945ba40da373a749601e55d
cronicasdemandacarudev-bit <cronicasdemandacarudev@gmail.com>
2026-05-20 23:20:16 -0300

    chore - arquivos-desnecessarios

--- cronicas-de-mandacaru-ppom/Script/pause_menu.gd
+++ cronicas-de-mandacaru-ppom/Script/pause_menu.gd
@@ -1,1 +1,30 @@
 extends CanvasLayer
+
+
+@onready var continue_button: Button = $menu_holder/continue_button
+
+
+
+func _ready():
+	visible = false
+
+
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event. is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+		continue_button.grab_focus()
+
+func _on_continue_button_pressed():
+	get_tree().paused = false
+	visible = false	
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	
+	
+	
